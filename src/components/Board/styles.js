160bb66e11718dd43c85a6b7cf830cbf5b95620c
Vinicius Monteiro <vmonteiro@gvdasa.com.br>
2019-07-30 13:59:28 -0300

    Initial commit

---
+++ src/components/Board/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 30px 0;
+  height: calc(100% - 80px);
+  display: flex;
+  flex-direction: row;
+`;
---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Label } from './styles';
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159C1" />
+      </header>
+
+      <p> oi </p>
+      <img src="https://api.adorable.io/avatars/285/abott@adorable.png" alt=""/>
+    </Container>
+  )
+}
