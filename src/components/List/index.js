d75a953b8954e52d679da9e377234c3152d2b274
Vinicius Monteiro <vmonteiro@gvdasa.com.br>
2019-08-04 23:20:34 -0300

    Implementa drag and drop

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import {Container} from './styles';
-import { MdAdd } from 'react-icons/md';
+import { Container } from "./styles";
+import { MdAdd } from "react-icons/md";
 
-import Card from '../Card';
+import Card from "../Card";
 
-export default function List({ data }) {
+export default function List({ data, listIndex }) {
   return (
-    <Container done={data.done} >
+    <Container done={data.done}>
       <header>
         <h2> {data.title} </h2>
-        { data.creatable &&
-        <button type="button"> 
-          <MdAdd size={24} color="#FFF" />
+        {data.creatable && (
+          <button type="button">
+            <MdAdd size={24} color="#FFF" />
           </button>
-        }
+        )}
       </header>
 
       <ul>
-        { data.cards.map(card => <Card key={card.id} data={card} />) }
+        {data.cards.map((card, index) => (
+          <Card key={card.id} data={card} index={index} listIndex={listIndex} />
+        ))}
       </ul>
     </Container>
-  )
+  );
 }
